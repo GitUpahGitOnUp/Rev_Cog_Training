81a34c24c226062de3c0a545f4c34961f76daa4b
GitUpahGitOnUp <angelaahutchison@gmail.com>
2026-09-08 15:11:46 -0400

    Bank project setup: sln, console app, and class library

---
+++ day6/Bank/bankAPP/Program.cs
@@ -0,0 +1,12 @@
+﻿using bankLIB;
+
+Accounts acc = new Accounts();
+acc.accBalance = 5000;
+acc.accName = "Ang";
+acc.accNo = 101;
+
+Console.WriteLine("Your Account Balance is:    " + acc.checkBalance());
+Console.WriteLine("After Withdrawal:   " + acc.Withdraw(800));
+Console.WriteLine("After Deposit:  " + acc.Deposit(12000));
+Console.WriteLine("");
+Console.WriteLine("Your New Balance is: " + acc.checkBalance());
---
+++ day6/Bank/bankLIB/Accounts.cs
@@ -0,0 +1,34 @@
+﻿namespace bankLIB;
+
+public class Accounts
+{
+    public int accNo {get; set;}
+
+    public double accBalance {get; set;}
+
+    public string accName {get; set;} = "";
+
+
+    public double checkBalance()
+    {
+        return accBalance;
+    }
+
+    public double Withdraw(int amount)
+    {
+        // add input validations HERE
+
+        // connect to DB and execute SQL query on table (using LINQ) 
+        accBalance = accBalance - amount;
+        return accBalance;
+    }
+
+    public double Deposit(int amount)
+    {
+        // add input validations HERE
+
+        // connect to DB and execute SQL query on table (using LINQ) 
+        accBalance = accBalance + amount;
+        return accBalance;
+    }
+}
